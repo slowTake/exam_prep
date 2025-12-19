b2284ef93465e3fe431462c6e7f2008d8971bd4a
pnurmi <pnurmi.zt@gmail.com>
2025-12-19 11:15:43 +0200

    added lvl2

--- filter/filter_1.c
+++ filter/filter_1.c
@@ -1,9 +1,8 @@
 #define _GNU_SOURCE
 
 #include <unistd.h>
-#include <string.h>
 #include <stdlib.h>
-#include <stdio.h>
+#include <string.h>
 
 #ifndef BUFFER_SIZE
 #define BUFFER_SIZE 42
@@ -12,7 +11,6 @@
 void print_stars(char *filter)
 {
 	int len = strlen(filter);
-
 	while(len > 0)
 	{
 		write(1, "*", 1);
@@ -23,29 +21,21 @@ void print_stars(char *filter)
 int process_input(char *filter)
 {
 	int bytes_read = 0;
-	char *input = NULL;
-	char *match;
-	char *current;
 	char buf[BUFFER_SIZE];
 	char *temp;
+	char *input = NULL;
 	int total_len = 0;
 
-	while((bytes_read = read(0, buf, BUFFER_SIZE))>0)
+	while((bytes_read = read(0, buf, BUFFER_SIZE)) > 0)
 	{
-		input = realloc(input, bytes_read + total_len);
-		// if(!temp)
-		// {
-		// 	free(input);
-		// 	perror("realloc failed");
-		// 	return(1);
-		// }
-		// printf("here1?");
-
-		memmove(input + total_len, buf, bytes_read + 1);
+		temp = realloc(input, total_len + bytes_read + 2);
+		input = temp;
+		memmove(input + total_len, buf, bytes_read);
 		total_len += bytes_read;
-		// input = temp;
 	}
-	current = input;
+
+	char *match;
+	char *current = input;
 
 	while(*current)
 	{
@@ -71,5 +61,4 @@ int main(int argc, char **argv)
 		return(1);
 	else
 		process_input(argv[1]);
-	return(0);
 }
