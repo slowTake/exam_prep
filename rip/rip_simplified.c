94eebe5442477efe1123233b25a02ef329808c20
Peter <pnurmi@gmail.com>
2026-01-06 20:52:42 +0200

    added simplified version

---
+++ rip/rip_simplified.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int is_balanced(char *str)
+{
+	int left = 0;
+	int right = 0;
+	int i = 0;
+
+	while(str[i])
+	{
+		if(str[i] == '(')
+			left++;
+		else if(str[i] == ')')
+		{
+			if(left > 0)
+				left--;
+			else
+				right++;
+		}
+		i++;
+	}
+	return(left == 0 && right == 0);
+}
+
+void solve(char *str, int index, int left, int right)
+{
+	if(str[index] == '\0')
+	{
+		if(left == 0 && right == 0 && is_balanced(str))
+			puts(str);
+		return;
+	}
+
+	char backup = str[index];
+	if(backup == '(' && left > 0)
+	{
+		str[index] = ' ';
+		solve(str, index + 1, left - 1, right);
+		str[index] = backup;
+	}
+	else if(backup == ')' && right > 0)
+	{
+		str[index] = ' ';
+		solve(str, index + 1, left, right - 1);
+		str[index] = backup;
+	}
+	solve(str, index + 1, left, right);
+}
+
+int main(int argc, char **argv)
+{
+	if(argc == 2 && argv[1][0])
+	{
+		int left = 0;
+		int right = 0;
+		int i = 0;
+		char *str = argv[1];
+		
+		while(str[i])
+		{
+			if(str[i] == '(')
+				left++;
+			else if(str[i] == ')')
+			{
+				if(left > 0)
+					left--;
+				else
+					right++;
+			}
+			i++;
+		}
+		solve(str, 0, left, right);
+	}
+	return(0);
+}
